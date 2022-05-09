1f32d7d3d52d9306a9a5714b5a7e9e4be6d8a8c5
zaki <zikoubouregbi@gmail.com>
2022-05-09 16:40:11 +0100

    new

--- DRINKS/drinks/drinks/settings.py
+++ DRINKS/drinks/drinks/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wok-(h^il7kt4ea2w-7s&-dk4l1q*vpb!xvc6l#h=o*$d8p91m'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
